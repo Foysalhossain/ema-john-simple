648230b16fa182ceaa8db3acb045d19e7be2d77f
Foysal Hossain <foysalhossain772@gmail.com>
2023-03-27 23:43:05 +0600

    added products

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Product.css';
+
+const Product = (props) => {
+
+    // Destructuring array of object
+    const { img, name, seller, ratings, price } = props.product
+
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className='product-info'>
+                <h6 className='product-name'>{name}</h6>
+                <p>Price: ${price}</p>
+                <p>Manufacturer: {seller}</p>
+                <p>Rating: {ratings} Start</p>
+            </div>
+            <button className='btn-cart'>Add to cart</button>
+        </div>
+    );
+};
+
+export default Product;
